cffcdcb271437a18eea648653a633357d8eb4972
Henrique Lira <henrique.lira@18digital.com.br>
2019-12-03 11:53:53 -0300

    Add reactotron integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,6 @@ module.exports = {
         'import/prefer-default-export': 'off',
         'react/static-property-placement': 'off',
         'react/state-in-constructor': 'off',
+        'no-console': ['error', { allow: ['tron'] }],
     },
 };
